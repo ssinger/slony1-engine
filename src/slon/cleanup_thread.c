c208e0f648b50d4889fe19d42c2d1fc42a822134
Jan Wieck <JanWieck@yahoo.com>
2004-02-20 15:28:49 +0000

    More comments.
    Jan

--- src/slon/cleanup_thread.c
+++ src/slon/cleanup_thread.c
@@ -6,7 +6,7 @@
  *	Copyright (c) 2003-2004, PostgreSQL Global Development Group
  *	Author: Jan Wieck, Afilias USA INC.
  *
- *	$Id: cleanup_thread.c,v 1.2 2004-02-20 15:13:28 wieck Exp $
+ *	$Id: cleanup_thread.c,v 1.3 2004-02-20 15:28:49 wieck Exp $
  *-------------------------------------------------------------------------
  */
 
@@ -47,6 +47,9 @@ cleanupThread_main(void *dummy)
 	struct timeval	tv_start;
 	struct timeval	tv_end;
 
+	/*
+	 * Connect to the local database
+	 */
 	if ((conn = slon_connectdb(rtcfg_conninfo, "local_cleanup")) == NULL)
 	{
 		kill(getpid(), SIGTERM);
@@ -54,9 +57,17 @@ cleanupThread_main(void *dummy)
 	}
 	dbconn = conn->dbconn;
 
+	/*
+	 * Build the query string for calling the cleanupEvent()
+	 * stored procedure
+	 */
 	dstring_init(&query1);
 	slon_mkquery(&query1, "select %s.cleanupEvent();", rtcfg_namespace);
 
+	/*
+	 * Build the query string for vacuuming replication
+	 * runtime data and event tables
+	 */
 	dstring_init(&query2);
 	slon_mkquery(&query2, 
 			"vacuum %s.sl_event; "
@@ -68,6 +79,9 @@ cleanupThread_main(void *dummy)
 			rtcfg_namespace, 
 			rtcfg_namespace);
 
+	/*
+	 * Loop until shutdown time arrived
+	 */
 	while (sched_wait_time(conn, SCHED_WAIT_SOCK_READ, 60000) == 0)
 	{
 		/*
@@ -89,7 +103,7 @@ printf("cleanupThread_main: %8.3f seconds for cleanupEvent()\n",
 TIMEVAL_DIFF(&tv_start, &tv_end));
 
 		/*
-		 * Detain the usual suspects
+		 * Detain the usual suspects (vacuum event and log data)
 		 */
 		gettimeofday(&tv_start, NULL);
 		res = PQexec(dbconn, dstring_data(&query2));
@@ -105,13 +119,22 @@ TIMEVAL_DIFF(&tv_start, &tv_end));
 		gettimeofday(&tv_end, NULL);
 printf("cleanupThread_main: %8.3f seconds for vacuuming\n",
 TIMEVAL_DIFF(&tv_start, &tv_end));
-
 	}
 
+	/*
+	 * Free Resources
+	 */
 	dstring_free(&query1);
 	dstring_free(&query2);
 
+	/*
+	 * Disconnect from the database
+	 */
 	slon_disconnectdb(conn);
+
+	/*
+	 * Terminate this thread
+	 */
 	pthread_exit(NULL);
 }
 
