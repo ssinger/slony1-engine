f1cf3becc396f17ab4f2a26e2965029c40c0a7cf
Christopher Browne <cbbrowne@ca.afilias.info>
2004-08-03 18:15:06 +0000

    Change this to look at all nodes and request function updates on them.
    (It was arbitrarily doing nodes 1 and 2, before...)

--- tools/altperl/update_node.pl
+++ tools/altperl/update_node.pl
@@ -1,17 +1,16 @@
 #!/usr/bin/perl
-# $Id: update_node.pl,v 1.1 2004-07-25 04:02:51 cbbrowne Exp $
+# $Id: update_node.pl,v 1.2 2004-08-03 18:15:06 cbbrowne Exp $
 # Author: Christopher Browne
 # Copyright 2004 Afilias Canada
 
 require 'slon-tools.pm';
 require 'slon.env';
 
-open(SLONIK, "|slonik");
-print SLONIK qq{
-        cluster name = $SETNAME ;
-        node 1 admin conninfo = '$CINFO1';
-        node 2 admin conninfo = '$CINFO2';
+open(SLONIK, ">/tmp/update_nodes.$$");
+print SLONIK genheader();
 
-	update functions (id = 1);
-	update functions (id = 2);
+foreach my $node (@NODES) {
+  print SLONIK "update functions (id = $node);\n";
 };
+close SLONIK;
+print `slonik /tmp/update_nodes.$$`;
