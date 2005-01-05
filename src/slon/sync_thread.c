f77ffad00271432ea4cc45ad57c09492997b61b1
Christopher Browne <cbbrowne@ca.afilias.info>
2005-01-05 22:22:53 +0000

    Additions and modifications to comments

--- src/slon/sync_thread.c
+++ src/slon/sync_thread.c
@@ -1,15 +1,18 @@
 /*-------------------------------------------------------------------------
  * sync_thread.c
  *
- *	Implementation of the thread generating SYNC evens.
+ *	Implementation of the thread that generates SYNC events.
  *
  *	Copyright (c) 2003-2004, PostgreSQL Global Development Group
  *	Author: Jan Wieck, Afilias USA INC.
  *
- *	$Id: sync_thread.c,v 1.13 2004-09-24 22:12:36 darcyb Exp $
+ *	$Id: sync_thread.c,v 1.14 2005-01-05 22:22:53 cbbrowne Exp $
  *-------------------------------------------------------------------------
  */
 
+
+/* Note that in 1.1, generate_sync_event() is a stored procedure that
+   does roughly the same thing as this... */
 
 #include <pthread.h>
 
@@ -51,43 +54,43 @@ syncThread_main(void *dummy)
 	PGconn         *dbconn;
 	PGresult       *res;
 	int             timeout_count;
-
+	
 	slon_log(SLON_DEBUG1,
-		 "syncThread: thread starts\n");
-
+			 "syncThread: thread starts\n");
+	
 	/*
 	 * Connect to the local database
 	 */
 	if ((conn = slon_connectdb(rtcfg_conninfo, "local_sync")) == NULL)
 		slon_abort();
 	dbconn = conn->dbconn;
-
+	
 	/*
 	 * We don't initialize the last known action sequence to the actual
 	 * value. This causes that we create a SYNC event allways on startup,
 	 * just in case.
 	 */
 	last_actseq_buf[0] = '\0';
-
+	
 	/*
 	 * Build the query that starts a transaction and retrieves the last
 	 * value from the action sequence.
 	 */
 	dstring_init(&query1);
 	slon_mkquery(&query1,
-		     "start transaction;"
-		     "set transaction isolation level serializable;"
-		     "select last_value from %s.sl_action_seq;",
-		     rtcfg_namespace);
-
+				 "start transaction;"
+				 "set transaction isolation level serializable;"
+				 "select last_value from %s.sl_action_seq;",
+				 rtcfg_namespace);
+	
 	/*
 	 * Build the query that calls createEvent() for the SYNC
 	 */
 	dstring_init(&query2);
 	slon_mkquery(&query2,
-		     "select %s.createEvent('_%s', 'SYNC', NULL);",
-		     rtcfg_namespace, rtcfg_cluster_name);
-
+				 "select %s.createEvent('_%s', 'SYNC', NULL);",
+				 rtcfg_namespace, rtcfg_cluster_name);
+	
 	timeout_count = (sync_interval_timeout == 0) ? 0 :
 		sync_interval_timeout - sync_interval;
 	while (sched_wait_time(conn, SCHED_WAIT_SOCK_READ, sync_interval) == SCHED_STATUS_OK)
@@ -100,14 +103,14 @@ syncThread_main(void *dummy)
 		if (PQresultStatus(res) != PGRES_TUPLES_OK)
 		{
 			slon_log(SLON_FATAL,
-				 "syncThread: \"%s\" - %s",
+					 "syncThread: \"%s\" - %s",
 			  dstring_data(&query1), PQresultErrorMessage(res));
 			PQclear(res);
 			slon_abort();
 			break;
 		}
 		/*
-		 * Check if it's identical to the last know seq or if the
+		 * Check if it's identical to the last known seq or if the
 		 * sync interval timeout has arrived.
 		 */
 		if (sync_interval_timeout != 0)
@@ -128,15 +131,15 @@ syncThread_main(void *dummy)
 			if (PQresultStatus(res) != PGRES_TUPLES_OK)
 			{
 				slon_log(SLON_FATAL,
-					 "syncThread: \"%s\" - %s",
-					 dstring_data(&query2), PQresultErrorMessage(res));
+						 "syncThread: \"%s\" - %s",
+						 dstring_data(&query2), PQresultErrorMessage(res));
 				PQclear(res);
 				slon_abort();
 				break;
 			}
 			slon_log(SLON_DEBUG2,
-			     "syncThread: new sl_action_seq %s - SYNC %s\n",
-				 last_actseq_buf, PQgetvalue(res, 0, 0));
+					 "syncThread: new sl_action_seq %s - SYNC %s\n",
+					 last_actseq_buf, PQgetvalue(res, 0, 0));
 			PQclear(res);
 
 			/*
@@ -146,8 +149,8 @@ syncThread_main(void *dummy)
 			if (PQresultStatus(res) != PGRES_COMMAND_OK)
 			{
 				slon_log(SLON_FATAL,
-				 "syncThread: \"commit transaction;\" - %s",
-					 PQresultErrorMessage(res));
+						 "syncThread: \"commit transaction;\" - %s",
+						 PQresultErrorMessage(res));
 				PQclear(res);
 				slon_abort();
 			}
@@ -168,8 +171,8 @@ syncThread_main(void *dummy)
 			if (PQresultStatus(res) != PGRES_COMMAND_OK)
 			{
 				slon_log(SLON_FATAL,
-				"syncThread: \"rollback transaction;\" - %s",
-					 PQresultErrorMessage(res));
+						 "syncThread: \"rollback transaction;\" - %s",
+						 PQresultErrorMessage(res));
 				PQclear(res);
 				slon_abort();
 			}
@@ -184,3 +187,11 @@ syncThread_main(void *dummy)
 	slon_log(SLON_DEBUG1, "syncThread: thread done\n");
 	pthread_exit(NULL);
 }
+
+/*
+ * Local Variables:
+ *  tab-width: 4
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ */
