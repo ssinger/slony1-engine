14d0bdf356e02379c92a8f227937827723aa8fc9
Christopher Browne <cbbrowne@ca.afilias.info>
2005-01-10 17:03:16 +0000

    Add usage output
    Potentially use environment to determine where cluster information is
    
    Turn reused logic into a function

--- tools/altperl/slon_kill.pl
+++ tools/altperl/slon_kill.pl
@@ -1,47 +1,61 @@
-#!perl # -*- perl -*-
-# $Id: slon_kill.pl,v 1.4 2004-09-09 17:04:07 cbbrowne Exp $
+#!/usr/bin/perl # -*- perl -*-
+# $Id: slon_kill.pl,v 1.5 2005-01-10 17:03:16 cbbrowne Exp $
 # Kill all slon instances for the current setname
 # Author: Christopher Browne
 # Copyright 2004 Afilias Canada
 
+use Getopt::Long;
+
+$SLON_ENV_FILE = 'slon.env'; # Where to find the slon.env file
+$SHOW_USAGE    = 0;          # Show usage, then quit
+
 require 'slon-tools.pm';
-require 'slon.env';
+require $SLON_ENV_FILE;
+
+my $USAGE =
+"Usage: slon_kill.pl [--config file]
+
+   Kills all running slon and slon_watchdog instances for the set
+   specified in the config file.
+
+";
+
+if ($SHOW_USAGE) {
+  print $USAGE;
+  exit 0;
+}
 
 print "slon_kill.pl...   Killing all slon and slon_watchdog instances for setname $SETNAME\n";
 print "1.  Kill slon watchdogs\n";
-#kill the watchdog
+# kill the watchdog
 
 open(PSOUT, ps_args() . " | egrep '[s]lon_watchdog' | sort -n | awk '{print \$2}'|");
-$found="n";
-while ($pid = <PSOUT>) {
-  chomp $pid;
-  if (!($pid)) {
-    print "No slon_watchdog is running for set $SETNAME!\n";
-  } else {
-    $found="y";
-    kill 9, $pid;
-    print "slon_watchdog for set $SETNAME killed - PID [$pid]\n";
-  }
-}
+shut_off_processes();
 close(PSOUT);
 if ($found eq 'n') {
   print "No watchdogs found\n";
 }
 print "\n2. Kill slon processes\n";
-#kill the slon daemon
+
+# kill the slon daemon
 $found="n";
 open(PSOUT, ps_args() . " | egrep \"[s]lon .*$SETNAME\" | sort -n | awk '{print \$2}'|");
-while ($pid = <PSOUT>) {
-  chomp $pid;
-  if (!($pid)) {
-    print "No Slon is running for set $SETNAME!\n";
-  } else {
-    kill 9, $pid;
-    print "Slon for set $SETNAME killed - PID [$pid]\n";
-    $found="y";
-  }
-}
+shut_off_processes();
 close(PSOUT);
 if ($found eq 'n') {
   print "No slon processes found\n";
+}
+
+sub shut_off_processes {
+  $found="n";
+  while ($pid = <PSOUT>) {
+    chomp $pid;
+    if (!($pid)) {
+      print "No slon_watchdog is running for set $SETNAME!\n";
+    } else {
+      $found="y";
+      kill 9, $pid;
+      print "slon_watchdog for set $SETNAME killed - PID [$pid]\n";
+    }
+  }
 }
