c12ea933b3094653a821833d1e43780fff84d242
Steve Simms <steve@deefs.net>
2005-03-11 18:55:44 +0000

    Added --config and --help to uninstall_nodes.pl (last script).
    According to the documentation for UNINSTALL NODE, it already handles
    the various commands that were subsequently being sent to psql, so
    I've removed them from this script.
    
    Instead of just running UNINSTALL NODE on $MASTERNODE, run it on all
    nodes, doing $MASTERNODE last.

--- tools/altperl/uninstall_nodes.pl
+++ tools/altperl/uninstall_nodes.pl
@@ -1,28 +1,40 @@
 #!@@PERL@@
-# $Id: uninstall_nodes.pl,v 1.7 2005-02-10 06:22:41 smsimms Exp $
+# $Id: uninstall_nodes.pl,v 1.8 2005-03-11 18:55:44 smsimms Exp $
 # Author: Christopher Browne
 # Copyright 2004 Afilias Canada
 
+use Getopt::Long;
+
+# Defaults
+$CONFIG_FILE = '@@SYSCONFDIR@@/slon_tools.conf';
+$SHOW_USAGE  = 0;
+
+# Read command-line options
+GetOptions("config=s" => \$CONFIG_FILE,
+	   "help"     => \$SHOW_USAGE);
+
+my $USAGE =
+"Usage: uninstall_nodes [--config file]
+
+    Removes Slony configuration from all nodes in a cluster.
+
+";
+
+if ($SHOW_USAGE) {
+  print $USAGE;
+  exit 0;
+}
+
 require '@@PGLIBDIR@@/slon-tools.pm';
-require '@@SYSCONFDIR@@/slon_tools.conf';
+require $CONFIG_FILE;
 
 $FILE="/tmp/slonik.$$";
 open(SLONIK, ">$FILE");
 print SLONIK genheader();
-print SLONIK qq{
-	uninstall node (id=$MASTERNODE);
-};
+foreach my $node (@NODES) {
+    next if $node == $MASTERNODE; # Do this one last
+    print SLONIK "  uninstall node (id=$node);\n";
+}
+print SLONIK "  uninstall node (id=$MASTERNODE);\n";
 close SLONIK;
 run_slonik_script($FILE);
-
-foreach my $node (@NODES) {
-  foreach my $command ("drop schema _$CLUSTER_NAME cascade;") {
-    print $command, "\n";
-    print `echo "$command" | psql -h $HOST[$node] -U $USER[$node] -d $DBNAME[$node] -p $PORT[$node]`;
-  }
-  foreach my $t (@SERIALTABLES) {
-    my $command = "alter table $t drop column \\\"_Slony-I_" . $CLUSTER_NAME . "_rowID\\\";";
-    print $command, "\n";
-    print `echo "$command" | psql -h $HOST[$node] -U $USER[$node] -d $DBNAME[$node] -p $PORT[$node]`;
-  }
-}
