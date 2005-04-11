86d3bdcd6a9b9478a6d3cc80a3be5929fe87f5cc
Christopher Browne <cbbrowne@ca.afilias.info>
2005-04-11 21:36:57 +0000

    Fix the comments to match the file

--- src/backend/slony1_base.v74.sql
+++ src/backend/slony1_base.v74.sql
@@ -1,12 +1,12 @@
 -- ----------------------------------------------------------------------
--- slony1_base.v73.sql
+-- slony1_base.v74.sql
 --
---    Version 7.3 specific parts of the basic replication schema.
+--    Version 7.4 specific parts of the basic replication schema.
 --
 --	Copyright (c) 2003-2004, PostgreSQL Global Development Group
 --	Author: Jan Wieck, Afilias USA INC.
 --
--- $Id: slony1_base.v74.sql,v 1.1 2004-03-12 23:17:31 wieck Exp $
+-- $Id: slony1_base.v74.sql,v 1.2 2005-04-11 21:36:57 cbbrowne Exp $
 -- ----------------------------------------------------------------------
 
 
