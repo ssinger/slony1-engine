94df25d5b0818f199ef11ff88539d795d0d1a192
Christopher Browne <cbbrowne@ca.afilias.info>
2005-04-13 20:39:32 +0000

    Address bug #1226
    If a set contains no tables, then warn the user that they may run afoul
    of bug #1226.

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -6,7 +6,7 @@
 --	Copyright (c) 2003-2004, PostgreSQL Global Development Group
 --	Author: Jan Wieck, Afilias USA INC.
 --
--- $Id: slony1_funcs.sql,v 1.15.2.13 2005-04-11 21:36:50 darcyb Exp $
+-- $Id: slony1_funcs.sql,v 1.15.2.14 2005-04-13 20:39:32 cbbrowne Exp $
 -- ----------------------------------------------------------------------
 
 
@@ -3449,6 +3449,16 @@ begin
 	if p_sub_receiver = p_sub_provider then
 		raise exception 
 				''Slony-I: set provider and receiver cannot be identical'';
+	end if;
+
+	-- ---
+	-- Check to see if the set contains any tables - gripe if not - bug #1226
+	-- ---
+	if not exists (select true 
+		from @NAMESPACE@.sl_table
+		where tab_set = p_sub_set) then
+		raise notice ''subscribeSet:: set % has no tables - risk of problems - see bug 1226'', p_sub_set;
+		raise notice ''http://gborg.postgresql.org/project/slony1/bugs/bugupdate.php?1226'';
 	end if;
 
 	-- ----
