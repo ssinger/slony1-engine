0fb05c51d2dde790a82a4acca5308ec822c2419d
Jan Wieck <JanWieck@yahoo.com>
2005-11-11 17:24:02 +0000

    Bumped version number in preparation of release.
    Jan

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -6,7 +6,7 @@
 --	Copyright (c) 2003-2004, PostgreSQL Global Development Group
 --	Author: Jan Wieck, Afilias USA INC.
 --
--- $Id: slony1_funcs.sql,v 1.64.2.13 2005-11-10 23:39:33 wieck Exp $
+-- $Id: slony1_funcs.sql,v 1.64.2.14 2005-11-11 17:23:58 wieck Exp $
 -- ----------------------------------------------------------------------
 
 
@@ -427,7 +427,7 @@ create or replace function @NAMESPACE@.slonyVersionPatchlevel()
 returns int4
 as '
 begin
-	return 2;
+	return 5;
 end;
 ' language plpgsql;
 comment on function @NAMESPACE@.slonyVersionPatchlevel () is 
