496180395e764c57ef97e06c5d7e86bec0305fbf
Christopher Browne <cbbrowne@ca.afilias.info>
2006-01-10 15:38:18 +0000

    Add slon command line documentation for -o option

--- src/slon/slon.c
+++ src/slon/slon.c
@@ -6,7 +6,7 @@
  *	Copyright (c) 2003-2006, PostgreSQL Global Development Group
  *	Author: Jan Wieck, Afilias USA INC.
  *
- *	$Id: slon.c,v 1.52.2.1 2006-01-06 17:07:47 cbbrowne Exp $
+ *	$Id: slon.c,v 1.52.2.2 2006-01-10 15:38:18 cbbrowne Exp $
  *-------------------------------------------------------------------------
  */
 
@@ -204,6 +204,7 @@ main(int argc, char *const argv[])
 		fprintf(stderr, "    -d <debuglevel>       verbosity of logging (1..4)\n");
 		fprintf(stderr, "    -s <milliseconds>     SYNC check interval (default 10000)\n");
 		fprintf(stderr, "    -t <milliseconds>     SYNC interval timeout (default 60000)\n");
+		fprintf(stderr, "    -o <milliseconds>     desired subscriber SYNC processing time\n");
 		fprintf(stderr, "    -g <num>              maximum SYNC group size (default 6)\n");
 		fprintf(stderr, "    -c <num>              how often to vacuum in cleanup cycles\n");
 		fprintf(stderr, "    -p <filename>         slon pid file\n");
