db2f293abced2a6193f795afd0f0c060ba56b3f6
Christopher Browne <cbbrowne@ca.afilias.info>
2006-04-11 20:22:29 +0000

    Added two new management scripts:
    - mkslonconf.sh - which rummages thru a Slony-I cluster and generates
      a slon-compatible .conf file for each node
    
    - launch_clusters.sh - which will invoke slons based on the above
      created .conf files
    
    Documentation included...

---
+++ tools/launch_clusters.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+# $Id: launch_clusters.sh,v 1.1 2006-04-11 20:22:28 cbbrowne Exp $
+# Cluster starter
+
+# This script should be run periodically to search for slon
+# "node[n].conf" configuration files, and make sure corresponding slon
+# daemons are running.
+
+# Environment variables needed:
+
+# PATH=/opt/dbs/pgsql74/bin:$PATH
+#
+#   The PATH must include, preferably at the beginning, the path to
+#   the slon binaries that are to be run
+#
+#SLHOME=/var/spool/slony1
+#   This indicates the "home" directory for slon configuration files
+#
+#LOGHOME=/var/log/slony1
+#   This indicates the "home" directory for slon log files
+#
+#   This indicates a list of clusters that are to be managed
+#
+# There are further expectations of directory structures under $SLHOME
+# and $LOGHOME, as follows:
+#
+# Under $SLHOME, there is a structure of "conf" files in the directory
+#   $SLHOME/$cluster/conf
+# with names like node1.conf, node2.conf, and so forth
+#
+# Under $SLHOME, there is also a directory for storing PID files thus
+# $SLHOME/$cluster/pid/node1.pid, $SLHOME/$cluster/pid/node2.pid, and
+# so forth
+#
+# Under $LOGHOME, logs will be appended, for each cluster, and each
+# node, into $LOGHOME/$cluster/node$nodenum/$cluster-node$nodenum.log
+#CLUSTERS="oxrsorg oxrslive oxrsaero oxrsin oxrsflex2 oxrsmobi"
+
+WATCHLOG="$LOGHOME/watcher.log"
+
+# This function provides a shorthand to log messages to
+log_action () {
+    LOGVALUE=$1
+    NOW=`date`
+    echo "$NOW $LOGVALUE" >> $WATCHLOG
+}
+ 
+invoke_slon () {
+    LOGHOME=$1
+    NODENUM=$2
+    CLUSTER=$3
+    SLONCONF=$4
+    log_action "run slon - slon -f $SLONCONF >> $LOGHOME/node${NODENUM}/${CLUSTER}-node${NODENUM}.log"
+    mkdir -p $LOGHOME/node${NODENUM}
+    slon -f $SLONCONF >> $LOGHOME/node${NODENUM}/${CLUSTER}-node${NODENUM}.log &
+}
+
+start_slon_if_needed () {
+    CONFIGPATH=$1
+    NODENUM=$2
+    LOGHOME=$3
+
+    if [[ -e $CONFIGPATH/conf/node${NODENUM}.conf ]] ; then
+	SLONCONF="$CONFIGPATH/conf/node${NODENUM}.conf"
+	SLONPIDFILE=`grep "^ *pid_file=" $SLONCONF | cut -d "=" -f 2 | cut -d "#" -f 1 | cut -d " " -f 1`
+	CLUSTER=`grep "^ *cluster_name=" $SLONCONF | cut -d "=" -f 2 | cut -d "'" -f 2`
+    else
+	log_action "Could not find node configuration in $CONFIGPATH/conf/node${NODENUM}.conf"
+	return
+    fi
+    if [[ -e $SLONPIDFILE ]] ; then
+	SLONPID=`cat $SLONPIDFILE`
+	FINDIT=`ps auxww $SLONPID | grep slon`
+	if [[ -z $FINDIT ]]; then
+        # Need to restart slon
+	    log_action "slon died for config $CONFIGPATH/conf/node${NODENUM}.conf"
+	    invoke_slon $LOGHOME $NODENUM $CLUSTER $SLONCONF
+	else
+	    echo "Slon already running - $SLONPID"
+	fi
+    else
+	invoke_slon $LOGHOME $NODENUM $CLUSTER $SLONCONF
+    fi
+}
+
+for cluster in `echo $CLUSTERS`; do
+    for conffile in `find $SLHOME/$cluster/conf -name "node[0-9]*.conf"`; do
+	# 1. Use sed to chop off all the path info
+        # 2. First cut to chop off up to "node"
+        # 3. Second cut to take off the ".conf" part
+        nodenum=`echo $conffile | sed 's/.*\///g' | cut -d "e" -f 2 | cut -d "." -f 1`
+	start_slon_if_needed $SLHOME/$cluster $nodenum $LOGHOME
+    done
+done
---
+++ tools/mkslonconf.sh
@@ -0,0 +1,197 @@
+#!/bin/sh
+# $Id: mkslonconf.sh,v 1.1 2006-04-11 20:22:28 cbbrowne Exp $
+# mkslonconf.sh
+# Build a set of slon.conf files compatible with launch_clusters.sh
+
+# Start with: 
+
+# a) Environment set up with libpq-compatible parameters to connect to
+#    a database
+# b) SLONYCLUSTER set to the appropriate cluster name
+# c) MKDESTINATION being a directory for conf/pid files to live in
+# d) LOGHOME being a directory for log directories to live in
+# SLONYCLUSTER=oxrsaero
+# MKDESTINATION=/tmp
+# LOGHOME=/tmp/logs
+
+echo "building slon config files in ${MKDESTINATION}/${SLONYCLUSTER}"
+mkdir -p ${MKDESTINATION}/${SLONYCLUSTER}
+mkdir -p ${MKDESTINATION}/${SLONYCLUSTER}/conf
+mkdir -p ${MKDESTINATION}/${SLONYCLUSTER}/pid
+echo "Make sure ${MKDESTINATION}/${SLONYCLUSTER}/conf, ${MKDESTINATION}/${SLONYCLUSTER}/pid exist"
+
+query="select pa_server, min(pa_conninfo) from \"_${SLONYCLUSTER}\".sl_path group by pa_server;"
+#echo "Query: $query"
+queryoutput="/tmp/slonconf.query.$$"
+psql --no-align --command "$query" --field-separator "|" --quiet --tuples-only > $queryoutput
+exec 3< $queryoutput
+
+create_conf_file ()
+{
+   node=$1
+   dsn=$2
+   conffile="${MKDESTINATION}/${SLONYCLUSTER}/conf/node${node}.conf"
+cat <<_EOF_ > $conffile
+# Sets how many cleanup cycles to run before a vacuum is done.
+# Range: [0,100], default: 3
+#vac_frequency=3
+
+# Debug log level (higher value ==> more output).  Range: [0,4], default 4
+#log_level=2
+
+# Check for updates at least this often in milliseconds.
+# Range: [10-60000], default 100
+#sync_interval=1000
+
+# Maximum amount of time in milliseconds before issuing a SYNC event, 
+# This prevents a possible race condition in which the action sequence 
+# is bumped by the trigger while inserting the log row, which makes 
+# this bump is immediately visible to the sync thread, but 
+# the resulting log rows are not visible yet.  If the sync is picked 
+# up by the subscriber, processed and finished before the transaction 
+# commits, this transaction's changes will not be replicated until the 
+# next SYNC.  But if all application activity suddenly stops, 
+# there will be no more sequence bumps, so the high frequent -s check 
+# won't detect that.  Thus, the need for sync_interval_timeout.
+# Range: [0-120000], default 1000
+#sync_interval_timeout=10000
+
+# Maximum number of SYNC events to group together when/if a subscriber
+# falls behind.  SYNCs are batched only if there are that many available 
+# and if they are contiguous. Every other event type in between leads to 
+# a smaller batch.  And if there is only one SYNC available, even -g60 
+# will apply just that one. As soon as a subscriber catches up, it will 
+# apply every single SYNC by itself.
+# Range:  [0,100], default: 6
+#sync_group_maxsize=6
+
+# Size above which an sl_log_? row's log_cmddata is considered large.
+# Up to 500 rows of this size are allowed in memory at once. Rows larger
+# than that count into the sync_max_largemem space allocated and free'd
+# on demand.
+# Range:  [1024,32768], default: 8192
+#sync_max_rowsize=8192
+
+# Maximum amount of memory allowed for large rows. Note that the algorithm
+# will stop fetching rows AFTER this amount is exceeded, not BEFORE. This
+# is done to ensure that a single row exceeding this limit alone does not
+# stall replication.
+# Range:  [1048576,1073741824], default: 5242880
+#sync_max_largemem=5242880
+
+# If this parameter is 1, messages go both to syslog and the standard 
+# output. A value of 2 sends output only to syslog (some messages will 
+# still go to the standard output/error).  The default is 0, which means 
+# syslog is off.  
+# Range:  [0-2], default: 0
+#syslog=0
+
+# If true, include the process ID on each log line.  Default is false.
+#log_pid=false
+
+# If true, include the timestamp on each log line.  Default is true.
+#log_timestamp=true
+
+# A strftime()-conformant format string for use with log timestamps.
+# Default is '%Y-%m-%d %H:%M:%S %Z'
+#log_timestamp_format='%Y-%m-%d %H:%M:%S %Z'
+
+# Where to write the pid file.  Default:  no pid file
+pid_file='${MKDESTINATION}/${SLONYCLUSTER}/pid/node${node}.pid'
+
+# Sets the syslog "facility" to be used when syslog enabled.  Valid 
+# values are LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, LOCAL7.
+#syslog_facility=LOCAL0
+
+# Sets the program name used to identify slon messages in syslog.
+#syslog_ident=slon
+
+# Set the cluster name that this instance of slon is running against
+# default is to read it off the command line
+cluster_name='${SLONYCLUSTER}'
+
+# Set slon's connection info, default is to read it off the command line
+conn_info='${dsn}'
+
+# maximum time planned for grouped SYNCs
+# If replication is behind, slon will try to increase numbers of
+# syncs done targetting that they should take this quantity of
+# time to process. in ms
+# Range [10000,600000], default 60000. 
+desired_sync_time=60000
+
+# Execute the following SQL on each node at slon connect time
+# useful to set logging levels, or to tune the planner/memory
+# settings.  You can specify multiple statements by seperating
+# them with a ;
+#sql_on_connection="SET log_min_duration_statement TO '1000';"
+
+# Command to run upon committing a log archive.
+# This command is passed one parameter, namely the full pathname of
+# the archive file
+#command_on_logarchive=""
+
+# A PostgreSQL value compatible with ::interval which indicates how
+# far behind this node should lag its providers.
+# lag_interval=""
+
+# Directory in which to stow sync archive files
+# archive_dir=""
+
+_EOF_
+mkdir -p $LOGHOME/$SLONYCLUSTER/node${node}
+
+echo " For node ${node}, created conf file $conffile"
+echo " as well as log directory $LOGHOME/$SLONYCLUSTER/node${node}"
+echo " -------------------------------------------------------------"
+
+}
+
+while read line <&3; do
+    #echo "Line: $line"
+    node=`echo ${line} | cut -d "|" -f 1`   
+    dsn=`echo ${line} | cut -d "|" -f 2`
+
+   #echo "node[$node]  dsn[$dsn]"
+    conffile="${MKDESTINATION}/${SLONYCLUSTER}/conf/node${node}.conf"
+    echo "Generating slon conf file $conffile"
+
+    if [[ -e $conffile ]] ; then
+	echo "config file $conffile already exists."
+	echo "Do you want to (Overwrite) it or (Skip) it (Anything else aborts) [Overwrite|Skip]?"
+	read nl
+	case nl in
+	    (Overwrite)
+	    echo "overwriting..."
+	    create_conf_file $node $dsn
+	    (Skip)
+	    echo "skipping..."
+	    (*)
+	    echo "invalid input - aborting..."
+	    exit -1
+	esac
+    else
+	echo "creating conf file for new node $node with DSN [$dsn]"
+	create_conf_file $node $dsn
+    fi
+done
+rm $queryoutput
+
+cat <<EOF 
+---------------------------------------------------------------------
+Be sure to review .conf files created in
+${MKDESTINATION}/${SLONYCLUSTER}/conf to ensure they are reasonable
+before starting up slons against them.  Customize as needed.
+
+Notably, if you have nodes which are reached via different DSNs from
+different locations, then the conn_info value may not be correct.
+
+In addition, this script will happily create .conf files for nodes
+which it found missing.  If you wanted nodes to be controlled from
+some other host, this could draw them in to the local host, which
+would not be what you wanted.  In most cases, this would only cause
+minor inconvenience; if you are running log shipping against a
+particular remote subscriber, this could cause you some real
+heartburn...
+---------------------------------------------------------------------
+EOF
