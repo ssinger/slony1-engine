46b14335f53947da8372d433108e58bea34087db
Christopher Browne <cbbrowne@ca.afilias.info>
2006-11-16 20:02:14 +0000

    Add a regexp exclusion scheme so that error messages that don't normally indicate problems (false positives) may be ignored.

--- tools/search-logs.sh
+++ tools/search-logs.sh
@@ -1,10 +1,14 @@
 #!/bin/sh 
-# $Id: search-logs.sh,v 1.1.2.1 2006-11-16 19:03:35 cbbrowne Exp $ 
+# $Id: search-logs.sh,v 1.1.2.2 2006-11-16 20:02:14 cbbrowne Exp $ 
  
 # Search logs for errors from the last hour 
 LOGHOME=${LOGHOME:-"/opt/logs"}    # Directory to search
 LOGRECIPIENT=${LOGRECIPIENT:-""}   # Email recipient - if not set, use STDOUT
 LOGTIMESTAMP=${LOGTIMESTAMP:-""}   # Override time - format should be "YYYY-MM-DD HH"
+
+# We found this error message came up often for reasons that will be resolved in 1.2
+# EXCLUSIONS='duplicate key violates unique constraint "sl_nodelock-pkey"'
+EXCLUSIONS="No Exclusions Known"
 
 if [[ -z $LOGTIMESTAMP ]] ; then
     HRRE=`date -d "1 hour ago" +"%Y-%m-%d %H:[0-9][0-9]:[0-9][0-9] ${TZ}"`
@@ -12,8 +16,9 @@ else
     HRRE="${LOGTIMESTAMP}:[0-9][0-9]:[0-9][0-9] ${TZ}"
 fi
 
-for log in `find ${LOGHOME} -name "*.log" -mmin -60 | egrep "/node[0-9]+/[^/]+.log"` ; do 
-    egrep "${HRRE} (ERROR|FATAL)" $log > /tmp/slony-errors.$$ 
+for log in `find ${LOGHOME} -name "*.log*" -mmin -60 | egrep "/node[0-9]+/[^/]+.log"` ; do 
+    egrep "${HRRE} (ERROR|FATAL)" $log | egrep -v "${EXCLUSIONS}" > /tmp/slony-errors.$$ 
+
     if [[ -s /tmp/slony-errors.$$ ]] ; then 
         echo "
 Errors in log ${log} 
