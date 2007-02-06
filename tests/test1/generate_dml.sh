6243cd28eb826635fa3c770036cd30646c8fd71f
Christopher Browne <cbbrowne@ca.afilias.info>
2007-02-06 16:01:38 +0000

    Add test for generate_sync_event() function to test1

--- tests/test1/generate_dml.sh
+++ tests/test1/generate_dml.sh
@@ -74,5 +74,12 @@ do_initdata()
   if [ $? -ne 0 ]; then
     warn 3 "do_initdata failed, see $mktmp/initdata.log for details"
   fi 
+  status "data load complete"
+
+  $pgbindir/psql -h $host -p $port -d $db -U $user -c "select \"_${CLUSTER1}\".generate_sync_event('1 second'::interval);" 1> $mktmp/gensync.log.1 2> $mktmp/gensync.log
+  rc=$?
+  if [ $rc -ne 0 ]; then
+      warn 3 "generate_sync_event() failed - rc=${rc} see $mktmp/gensync.log* for details"
+  fi
   status "done"
 }
