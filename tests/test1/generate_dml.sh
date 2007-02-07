b6ce5a19f0ddda48156c16841e283bbc2b110b75
Christopher Browne <cbbrowne@ca.afilias.info>
2007-02-07 19:16:02 +0000

    Add tests of functions generate_sync_event() and make_function_strict()

--- tests/test1/generate_dml.sh
+++ tests/test1/generate_dml.sh
@@ -30,6 +30,7 @@ generate_initdata()
   status "generating ${numrows} transactions of random data"
   percent=`expr $j \* 5`
   status "$percent %"
+  GENDATA="$mktmp/generate.data"
   while : ; do
     txtalen=$(random_number 1 100)
     txta=$(random_string ${txtalen})
@@ -37,10 +38,13 @@ generate_initdata()
     txtblen=$(random_number 1 100)
     txtb=$(random_string ${txtblen})
     txtb=`echo ${txtb} | sed -e "s/\\\\\\\/\\\\\\\\\\\\\\/g" -e "s/'/''/g"`
-    GENDATA="$mktmp/generate.data"
+    ra=$(random_number 1 9)
+    rb=$(random_number 1 9)
+    rc=$(random_number 1 9)
     echo "INSERT INTO table1(data) VALUES ('${txta}');" >> $GENDATA
     echo "INSERT INTO table2(table1_id,data) SELECT id, '${txtb}' FROM table1 WHERE data='${txta}';" >> $GENDATA
     echo "INSERT INTO table3(table2_id) SELECT id FROM table2 WHERE data ='${txtb}';" >> $GENDATA
+    echo "INSERT INTO table5(numcol,realcol,ptcol,pathcol,polycol,circcol,ipcol,maccol,bitcol) values ('${ra}${rb}.${rc}','${ra}.${rb}${rc}','(${ra},${rb})','((${ra},${ra}),(${rb},${rb}),(${rc},${rc}),(${ra},${rc}))','((${ra},${rb}),(${rc},${ra}),(${rb},${rc}),(${rc},${rb}))','<(${ra},${rb}),${rc}>','192.168.${ra}.${rb}${rc}','08:00:2d:0${ra}:0${rb}:0${rc}',X'${ra}${rb}${rc}');" >> $GENDATA
     if [ ${i} -ge ${numrows} ]; then
       break;
     else
@@ -58,9 +62,9 @@ generate_initdata()
 
 do_initdata()
 {
-   originnode=${ORIGINNODE:-"1"}
+  originnode=${ORIGINNODE:-"1"}
   eval db=\$DB${originnode}
-   eval host=\$HOST${originnode}
+  eval host=\$HOST${originnode}
   eval user=\$USER${originnode}
   eval port=\$PORT${originnode}
   generate_initdata
@@ -76,6 +80,11 @@ do_initdata()
   rc=$?
   if [ $rc -ne 0 ]; then
       warn 3 "generate_sync_event() failed - rc=${rc} see $mktmp/gensync.log* for details"
+  fi
+  $pgbindir/psql -h $host -p $port -d $db -U $user -c "select \"_${CLUSTER1}\".make_function_strict('xxidin', '(cstring)');" 1> $mktmp/gensync.log.1 2> $mktmp/gensync.log
+  rc=$?
+  if [ $rc -ne 0 ]; then
+      warn 3 "make_function_strict('xxidin', '(cstring)') failed - rc=${rc} see $mktmp/gensync.log* for details"
   fi
   status "done"
 }
