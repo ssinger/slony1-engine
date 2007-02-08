050373ed611b8d3b933246f97d53ffe184009733
Christopher Browne <cbbrowne@ca.afilias.info>
2007-02-08 22:50:02 +0000

    Add status reporting on the two new tests (on generate_sync_event() and make_function_strict()).

--- tests/test1/generate_dml.sh
+++ tests/test1/generate_dml.sh
@@ -81,10 +81,12 @@ do_initdata()
   if [ $rc -ne 0 ]; then
       warn 3 "generate_sync_event() failed - rc=${rc} see $mktmp/gensync.log* for details"
   fi
+  status "completed generate_sync_event() test"
   $pgbindir/psql -h $host -p $port -d $db -U $user -c "select \"_${CLUSTER1}\".make_function_strict('xxidin', '(cstring)');" 1> $mktmp/gensync.log.1 2> $mktmp/gensync.log
   rc=$?
   if [ $rc -ne 0 ]; then
       warn 3 "make_function_strict('xxidin', '(cstring)') failed - rc=${rc} see $mktmp/gensync.log* for details"
   fi
+  status "completed make_function_strict() test"
   status "done"
 }
