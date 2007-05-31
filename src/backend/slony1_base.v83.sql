084c2e0ea8643dc071bf91fa365428c5e3c67a1a
Jan Wieck <JanWieck@yahoo.com>
2007-05-31 18:55:28 +0000

    Well, someone might miss these two ...
    Jan

---
+++ src/backend/slony1_base.v83.sql
@@ -0,0 +1,12 @@
+-- ----------------------------------------------------------------------
+-- slony1_base.v83.sql
+--
+--    Version 8.3 specific parts of the basic replication schema.
+--
+--	Copyright (c) 2007, PostgreSQL Global Development Group
+--	Author: Jan Wieck, Afilias USA INC.
+--
+-- $Id: slony1_base.v83.sql,v 1.1 2007-05-31 18:55:28 wieck Exp $
+-- ----------------------------------------------------------------------
+
+
---
+++ src/backend/slony1_funcs.v83.sql
@@ -0,0 +1,11 @@
+-- ----------------------------------------------------------------------
+-- slony1_funcs.v83.sql
+--
+--    Version 8.3 specific part of the replication support functions.
+--
+--	Copyright (c) 2007, PostgreSQL Global Development Group
+--	Author: Jan Wieck, Afilias USA INC.
+--
+-- $Id: slony1_funcs.v83.sql,v 1.1 2007-05-31 18:55:28 wieck Exp $
+-- ----------------------------------------------------------------------
+
