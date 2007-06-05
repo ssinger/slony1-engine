c1f64a7a61ccec9c2eacad99c9cb6944382621b8
Christopher Browne <cbbrowne@ca.afilias.info>
2007-06-05 14:38:06 +0000

    Add a series of STORE TRIGGER requests so as to hopefully have multiple non-SYNC events in a row to test

--- tests/testlogship/exec_ddl.sh
+++ tests/testlogship/exec_ddl.sh
@@ -1,8 +1,13 @@
 testname=$1
 echo "
+  store trigger (table id=1, trigger name='nonexistant trigger', event node=1);
+  store trigger (table id=1, trigger name='another nonexistant trigger', event node=1);
+  store trigger (table id=1, trigger name='still another nonexistant trigger', event node=1);
   EXECUTE SCRIPT (
        SET ID = 1,
        FILENAME = '${testname}/ddl_updates.sql',
        EVENT NODE = 1
     );
+  store trigger (table id=1, trigger name='late, another nonexistant trigger', event node=1);
+  store trigger (table id=1, trigger name='late, still another nonexistant trigger', event node=1);
 "
