d6324ea3e00dcef505aa76e3b8e4c5634d9fe809
Christopher Browne <cbbrowne@ca.afilias.info>
2007-06-13 15:56:13 +0000

    Patch to slony1_dump.sh per <stephane.schildknecht@postgresqlfr.org>
    Put "E" into "select E'\\\\\\\\.';" for better encoding a set of "\\backslashes\\"

--- tools/slony1_dump.sh
+++ tools/slony1_dump.sh
@@ -2,7 +2,7 @@
 # ----------
 # slony1_dump.sh
 #
-# $Id: slony1_dump.sh,v 1.8 2006-06-20 18:48:07 cbbrowne Exp $
+# $Id: slony1_dump.sh,v 1.8.2.1 2007-06-13 15:56:13 cbbrowne Exp $
 #
 #	This script creates a special data only dump from a subscriber
 #	node. The stdout of this script, fed into psql for a database that
@@ -187,7 +187,7 @@ echo "set transaction isolation level serializable;"
 # ----
 echo "select 'copy $clname.sl_sequence_offline from stdin;';"
 echo "select seq_id::text || '	' || seq_relname  || '	' || seq_nspname from $clname.sl_sequence;"
-printf "select '\\\\\\\\.';"
+printf "select E'\\\\\\\\.';"
 
 for seq in $sequences ; do
 	eval seqname=\$seqname_$seq
@@ -210,12 +210,11 @@ echo "select 'insert into $clname.sl_setsync_offline values (' ||
 system_type=`uname`
 for tab in $tables ; do
 	eval tabname=\$tabname_$tab
-
 	# Get fieldnames...
  	fields=`psql -At -c "select $clname.copyfields($tab);" $dbname`
  	echo "select 'copy $tabname $fields from stdin;';"
 	echo "copy $tabname $fields to stdout;"
- 	printf "select '\\\\\\\\.';"
+ 	printf "select E'\\\\\\\\.';"
 done
 
 # ----
