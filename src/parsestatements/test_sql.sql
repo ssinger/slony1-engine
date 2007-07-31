979236cf4e1ceab563f812419e333bea9621a221
Christopher Browne <cbbrowne@ca.afilias.info>
2007-07-31 14:55:34 +0000

    Fixes to DDL statement parser, along with additional torture tests provided by Weslee Bilodeau

--- src/parsestatements/test_sql.sql
+++ src/parsestatements/test_sql.sql
@@ -35,3 +35,43 @@ create or replace function foo (text) returns integer as $$
     return NULL;
   end;
 $$ language plpgsql;
+
+
+-- Here is a rule creation with an embedded semicolon
+-- "Dmitry Koterov" <dmitry@koterov.ru>
+
+create table "public"."position";
+
+CREATE RULE "position_get_last_id_on_insert2"
+AS ON INSERT TO "public"."position" DO (SELECT
+currval('position_position_id_seq'::regclass) AS id;);
+
+-- Added to verify handling of queries tried by
+-- "Dmitry Koterov" <dmitry@koterov.ru>
+
+CREATE INDEX aaa ON public.bbb USING btree ((-ccc), ddd);
+
+--  Apparently a pair of backslashes fold down into one?
+-- "Dmitry Koterov" <dmitry@koterov.ru>
+
+CREATE UNIQUE INDEX "i_dictionary_uni_abbr" ON "static"."dictionary"
+USING btree ((substring(dic_russian, E'^([^(]*[^( ]) *\\('::text)))
+WHERE (dic_category_id = 26);
+
+-- Some more torturing per Weslee Bilodeau
+
+-- I figure the $_$, $$, etc edge-casees would be another fun one to roll
+-- into a custom parser.
+
+CREATE FUNCTION test( ) RETURNS text AS $_$ SELECT ';', E'\';\'',
+'"";""', E'"\';' ; SELECT 'OK'::text ; $_$ LANGUAGE SQL ;
+
+SELECT $_$ hello; this ; - is '\" a '''' test $_$ ;
+
+SELECT $$ $ test ; $ ;  $$ ;
+
+-- All really funky, but perfectly valid.
+
+-- Force a query to be at the end...
+
+create table foo;
