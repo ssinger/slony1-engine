652dae5723e1d954b601a9fa499d1de07eb81d32
Christopher Browne <cbbrowne@ca.afilias.info>
2007-08-03 16:16:08 +0000

    Update version numbers to 1.2.11

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -6,7 +6,7 @@
 --	Copyright (c) 2003-2004, PostgreSQL Global Development Group
 --	Author: Jan Wieck, Afilias USA INC.
 --
--- $Id: slony1_funcs.sql,v 1.98.2.18 2007-07-29 17:27:33 wieck Exp $
+-- $Id: slony1_funcs.sql,v 1.98.2.19 2007-08-03 16:16:08 cbbrowne Exp $
 -- ----------------------------------------------------------------------
 
 -- **********************************************************************
@@ -430,7 +430,7 @@ create or replace function @NAMESPACE@.slonyVersionPatchlevel()
 returns int4
 as '
 begin
-	return 10;
+	return 11;
 end;
 ' language plpgsql;
 comment on function @NAMESPACE@.slonyVersionPatchlevel () is 
