b12ab4e564506b5928ab091bf24ef7c19345653b
Christopher Browne <cbbrowne@ca.afilias.info>
2007-10-04 15:31:44 +0000

    Add a negative test to DDL script test: try to run some wrong SQL

--- tests/testddl/exec_ddl.sh
+++ tests/testddl/exec_ddl.sh
@@ -5,4 +5,17 @@ echo "
        FILENAME = '${testname}/ddl_updates.sql',
        EVENT NODE = 1
     );
+
+  try {
+       execute script (
+            set id=1,
+            filename='${testname}/bad_ddl.sql',
+            event node=1
+       );
+  } on error {
+     echo 'a bad DDL script gets rolled back on the master';
+  } on success {
+     echo 'a bad DDL script did not get rolled back - ERROR!';
+     exit -1;
+  }
 "
