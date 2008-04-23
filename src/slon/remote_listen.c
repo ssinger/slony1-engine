49aeb2ce94752dbf7f6c92a4cba06288a819dc2c
Christopher Browne <cbbrowne@ca.afilias.info>
2008-04-23 20:37:06 +0000

    EEP!  A change I described in an email got included into the pgindent commit!
    Rolling that back...

--- src/slon/remote_listen.c
+++ src/slon/remote_listen.c
@@ -7,7 +7,7 @@
  *	Copyright (c) 2003-2004, PostgreSQL Global Development Group
  *	Author: Jan Wieck, Afilias USA INC.
  *
- *	$Id: remote_listen.c,v 1.41 2008-04-23 20:35:43 cbbrowne Exp $
+ *	$Id: remote_listen.c,v 1.42 2008-04-23 20:37:06 cbbrowne Exp $
  * ----------------------------------------------------------------------
  */
 
@@ -697,7 +697,7 @@ remoteListen_receive_events(SlonNode * node, SlonConn * conn,
 	{
 		slon_appendquery(&query, ")");
 	}
-	slon_appendquery(&query, " order by e.ev_origin, e.ev_seqno limit 2000");
+	slon_appendquery(&query, " order by e.ev_origin, e.ev_seqno");
 
 	rtcfg_unlock();
 
