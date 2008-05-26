c9bab9151c8a0c62ffee249279833a087f77e209
Christopher Browne <cbbrowne@ca.afilias.info>
2008-05-26 18:24:21 +0000

    Ran INDENT on C code for random number generation for tests

--- tests/random_number.c
+++ tests/random_number.c
@@ -1,15 +1,20 @@
-/* $Id: random_number.c,v 1.2 2007-06-06 22:20:39 cbbrowne Exp $ */
+/* $Id: random_number.c,v 1.3 2008-05-26 18:24:21 cbbrowne Exp $ */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char *argv[])
+int
+main(int argc, const char *argv[])
 {
-	int lower, upper;
-	int base, result;
-	if (argc != 3) {
+	int			lower,
+				upper;
+	int			base,
+				result;
+
+	if (argc != 3)
+	{
 		printf("args: %d - random_number: lower_limit upper_limit\n", argc);
-		exit (1);
+		exit(1);
 	}
 	sscanf(argv[1], "%d", &lower);
 	sscanf(argv[2], "%d", &upper);
--- tests/random_string.c
+++ tests/random_string.c
@@ -1,20 +1,25 @@
-/* $Id: random_string.c,v 1.2 2007-06-06 22:20:39 cbbrowne Exp $ */
+/* $Id: random_string.c,v 1.3 2008-05-26 18:24:21 cbbrowne Exp $ */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char *argv[])
+int
+main(int argc, const char *argv[])
 {
-	int length, i;
-	int base, result;
-	if (argc != 2) {
+	int			length,
+				i;
+	int			base,
+				result;
+
+	if (argc != 2)
+	{
 		printf("args: %d - random_string: length\n", argc);
-		exit (1);
+		exit(1);
 	}
 	sscanf(argv[1], "%d", &length);
 
 	srand(time(0));
-	for (i = 0; i< length; i++) 
-		printf("%c", (rand() % (122-48))+48);
+	for (i = 0; i < length; i++)
+		printf("%c", (rand() % (122 - 48)) + 48);
 	printf("\n", result);
 }
