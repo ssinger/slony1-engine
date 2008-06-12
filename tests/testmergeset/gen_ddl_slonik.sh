8364617dc7375585c95bf4f6c64e3cf8b26de099
Christopher Browne <cbbrowne@ca.afilias.info>
2008-06-12 18:17:49 +0000

    Fix up WAIT FOR EVENT requests

--- tests/testmergeset/gen_ddl_slonik.sh
+++ tests/testmergeset/gen_ddl_slonik.sh
@@ -13,14 +13,17 @@ echo "
 
   set add table (set id=999, origin=1, id=${tableid}, fully qualified name='public.sales_txns_${year}_${month}');
   subscribe set (id=999, provider=1, receiver=2, forward = yes);
+  wait for event (origin=2, confirmed=1, wait on=1);
   sync (id=1);
-  wait for event (origin=1, confirmed=2, wait on=1);
+  wait for event (origin=1, confirmed=2, wait on=2);
   subscribe set (id=999, provider=1, receiver=3, forward = no);
+  wait for event (origin=3, confirmed=1, wait on=1);
   sync (id=1);
-  wait for event (origin=1, confirmed=3, wait on=1);
+  wait for event (origin=1, confirmed=3, wait on=3);
   subscribe set (id=999, provider=2, receiver=4, forward = no);
+  wait for event (origin=4, confirmed=1, wait on=1);
   sync (id=1);
-  wait for event (origin=1, confirmed=ALL, wait on=1);
+  wait for event (origin=1, confirmed=4, wait on=4);
   merge set (ID = 1, ADD ID = 999, ORIGIN = 1 );
 
 " 
