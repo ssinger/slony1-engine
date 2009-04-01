b13c52cc5a68724afece9f75928815a539d94536
Christopher Browne <cbbrowne@ca.afilias.info>
2009-04-01 19:03:51 +0000

    Improve documentation of what MERGE SET does, per Mark Stosberg

--- tools/altperl/slonik_merge_sets.pl
+++ tools/altperl/slonik_merge_sets.pl
@@ -1,5 +1,5 @@
 #!@@PERL@@
-# $Id: slonik_merge_sets.pl,v 1.3 2008-02-14 16:41:35 cbbrowne Exp $
+# $Id: slonik_merge_sets.pl,v 1.4 2009-04-01 19:03:51 cbbrowne Exp $
 # Author: Christopher Browne
 # Copyright 2004 Afilias Canada
 
@@ -14,9 +14,10 @@ GetOptions("config=s" => \$CONFIG_FILE,
 	   "help"     => \$SHOW_USAGE);
 
 my $USAGE =
-"Usage: merge_sets [--config file] node# set# set#
+"Usage: merge_sets [--config file] node# targetset# srcset#
 
     Merges the contents of the second set into the first one.
+    When completed, the second set no longer exists.
 
 ";
 
@@ -59,9 +60,9 @@ $slonik .= genheader();
 $slonik .= "  try {\n";
 $slonik .= "    merge set (id = $set1, add id = $set2, origin = $node);\n";
 $slonik .= "  } on error {\n";
-$slonik .= "    echo 'Failure to merge sets $set1 and $set2 with origin $node';\n";
+$slonik .= "    echo 'Failure to merge set $set2 into $set1 with origin $node';\n";
 $slonik .= "    exit 1;\n";
 $slonik .= "  }\n";
-$slonik .= "  echo 'Replication set $set2 merged in with $set1 on origin $node';\n";
+$slonik .= "  echo 'Replication set $set2 merged into $set1 on origin $node. Set $set2 no longer exists.';\n";
 
 run_slonik_script($slonik, 'MERGE SET');
