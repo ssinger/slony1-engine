4b2019af6c59c9b5fc5d9adc10e37f7a34aab2b1
Christopher Browne <cbbrowne@ca.afilias.info>
2009-07-08 20:46:02 +0000

    Change version from 1.2.16 to 1.2.17, add notes to RELEASE

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -6,7 +6,7 @@
 --	Copyright (c) 2003-2004, PostgreSQL Global Development Group
 --	Author: Jan Wieck, Afilias USA INC.
 --
--- $Id: slony1_funcs.sql,v 1.98.2.34 2009-04-30 16:20:27 cbbrowne Exp $
+-- $Id: slony1_funcs.sql,v 1.98.2.35 2009-07-08 20:46:02 cbbrowne Exp $
 -- ----------------------------------------------------------------------
 
 -- **********************************************************************
@@ -430,7 +430,7 @@ create or replace function @NAMESPACE@.slonyVersionPatchlevel()
 returns int4
 as '
 begin
-	return 16;
+	return 17;
 end;
 ' language plpgsql;
 comment on function @NAMESPACE@.slonyVersionPatchlevel () is 
