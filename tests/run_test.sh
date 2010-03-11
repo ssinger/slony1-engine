e2905d181ec75b4e5162db698ff83d9c2ba2b024
Christopher Browne <cbbrowne@ca.afilias.info>
2010-03-11 21:40:08 +0000

    Improve error reporting to indicate DB conn info in case where failure is due to bad conn info

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: run_test.sh,v 1.26.2.4 2009-07-21 21:17:44 cbbrowne Exp $
+# $Id: run_test.sh,v 1.26.2.5 2010-03-11 21:40:08 cbbrowne Exp $
 
 pgbindir=${PGBINDIR:-"/usr/local/pgsql/bin"}
 numerrors=0
@@ -54,8 +54,11 @@ echo "----------------------------------------------------"
 
 #load settings
 
+echo "Loading default settings"
 . settings.ik
+echo "Loading test-specific settings"
 . $testname/settings.ik
+echo "Loading support functions"
 . support_funcs.sh
 
 echo "Test by ${SLONYTESTER} to be summarized in ${SLONYTESTFILE}"
@@ -217,7 +220,7 @@ store_path()
           if [ -n "${bdb}" -a "${bhost}" -a "${buser}" -a "${bport}" ]; then
 	    echo "STORE PATH (SERVER=@node${i}, CLIENT=@node${j}, CONNINFO='dbname=${db} host=${host} user=${buser} port=${port}');" >> $mktmp/slonik.script
           else
-            err 3 "No conninfo"
+            err 3 "No conninfo - bdb=${bdb} bhost=${bhost} user=${buser} bport=${bport}"
           fi
         fi
         if [ ${j} -ge ${NUMNODES} ]; then
