736fe39ec6ef51472c2bb59425c5dbee67479a37
Christopher Browne <cbbrowne@ca.afilias.info>
2010-08-24 18:31:28 -0400

    Mark version 2.0.5, as that's what the present stream of changes will get tagged as.

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -433,7 +433,7 @@ create or replace function @NAMESPACE@.slonyVersionPatchlevel()
 returns int4
 as $$
 begin
-	return 4;
+	return 5;
 end;
 $$ language plpgsql;
 comment on function @NAMESPACE@.slonyVersionPatchlevel () is 
