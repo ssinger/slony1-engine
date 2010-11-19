bc480a4eefe236016b8eac31486b7b083aea1912
Steve Singer <ssinger@ca.afilias.info>
2010-11-19 16:29:21 -0500

    Adding .sql files for .v84 commit 0d1781229fd3cc6a2abac083174166945d08ed7f  did not update xxid and introduce a 1.2 base_funcs file for v.84 even thought it brought over a 8.4 compatibility mode

---
+++ src/backend/slony1_base.v84.sql
@@ -0,0 +1,12 @@
+-- ----------------------------------------------------------------------
+-- slony1_base.v84.sql
+--
+--    Version 8.4 specific parts of the basic replication schema.
+--
+--	Copyright (c) 2003-2009, PostgreSQL Global Development Group
+--	Author: Jan Wieck, Afilias USA INC.
+--
+-- 
+-- ----------------------------------------------------------------------
+
+
