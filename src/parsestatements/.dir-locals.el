6572ed11b870c0092ecbccab706f1bed80976bf1
Christopher Browne <cbbrowne@ca.afilias.info>
2011-02-24 17:50:13 -0500

    Add emacs directory locals

---
+++ src/parsestatements/.dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (indent-tabs-mode . t)
+  (tab-width . 4)
+  (fill-column . 80))
+ (c-mode
+  (c-indent-level . 4)
+  (c-basic-offset . 4)
+  (c-file-style . "BSD")))
