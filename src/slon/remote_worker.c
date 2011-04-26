ssinger/slony1-engine

commit a0729b759306dd21fe4043ee52a80fae5b515c90

Obtaining this lock actually increases deadlock because it creates a conflict between sl_config_lock and sl_event_lock.
slonik initiated queries will always obtain sl_event_lock before
sl_config_lock (or at least when creating events).  Having
remote_worker do the opposite isn't a good idea.

If we find more examples of deadlock with this removed then
they need to be adressed in some other fashion.

src/slon/remote_worker.c
@@ -720,16 +720,7 @@ remoteWorkerThread_main(void *cdata)
 		}
 		else	/* not SYNC */
 		{
-			/*
-			 * Avoid deadlock problems during configuration changes by locking
-			 * the central configuration lock right from the start.
-			 */
-			slon_appendquery(&query1,
-							 "lock table %s.sl_config_lock; ",
-							 rtcfg_namespace);
-			if (query_execute(node, local_dbconn, &query1) < 0)
-				slon_retry();
-			dstring_reset(&query1);
+		
 
 			/*
 			 * For all non-SYNC events, we write at least a standard event

commit 66917bf98e3ccd12f16eb2c786025b2d8f774258

Fixing bug introduced in a0729b759306dd21fe4043ee52a80fae5b515c90 query1 has a 'start transaction;' command that needs to be executed otherwise no transaction is started.  This makes rolling back on errors hard.

src/slon/remote_worker.c
@@ -721,7 +721,12 @@ remoteWorkerThread_main(void *cdata)
 		else	/* not SYNC */
 		{
 		
-
+			/**
+			 * open the transaction.
+			 */
+			if (query_execute(node, local_dbconn, &query1) < 0)
+				slon_retry();
+			dstring_reset(&query1);
 			/*
 			 * For all non-SYNC events, we write at least a standard event
 			 * tracking log file and adjust the ssy_seqno in our internal