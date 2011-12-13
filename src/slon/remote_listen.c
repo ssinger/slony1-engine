44aa04906a195b90a9e5fd6552d6fe097bc5a50d
Steve Singer <ssinger@ca.afilias.info>
2011-12-13 11:49:14 -0500

    Bug 255. Changes for 9.1 to avoid pivot conflicts.
    specify SERIALIZABLE for read only deferred transactions.
    Make remote_worker calls to the provider read-only deferred
    Make remote_worker calls on the local con READ COMMITTED.
    Make the remote listener run in a READ ONLY DEFERRED transaction on
    
    Based on a patch by Simon Riggs.

--- src/slon/remote_listen.c
+++ src/slon/remote_listen.c
@@ -293,6 +293,52 @@ remoteListenThread_main(void *cdata)
 
 				continue;
 			}
+			if(PQserverVersion(dbconn) >= 90100) 
+			{
+				slon_mkquery(&query1,"SET SESSION CHARACTERISTICS AS TRANSACTION read only deferrable");
+				res = PQexec(dbconn, dstring_data(&query1));
+				if (PQresultStatus(res) != PGRES_COMMAND_OK)
+				{
+					slon_log(SLON_ERROR,
+							 "remoteListenThread_%d: \"%s\" - %s",
+							 node->no_id,
+							 dstring_data(&query1), PQresultErrorMessage(res));
+					PQclear(res);
+					slon_disconnectdb(conn);
+					free(conn_conninfo);
+					conn = NULL;
+					conn_conninfo = NULL;
+					rc = sched_msleep(node, pa_connretry * 1000);
+					if (rc != SCHED_STATUS_OK && rc != SCHED_STATUS_CANCEL)
+						break;
+					
+					continue;
+				}						
+			
+			}
+			if(PQserverVersion(dbconn) >= 90100)
+			{
+				slon_mkquery(&query1,"SET SESSION CHARACTERISTICS AS TRANSACTION read only isolation level serializable deferrable");
+				res = PQexec(dbconn, dstring_data(&query1));
+				if (PQresultStatus(res) != PGRES_COMMAND_OK)
+				{
+					slon_log(SLON_ERROR,
+							 "remoteListenThread_%d: \"%s\" - %s",
+							 node->no_id,
+							 dstring_data(&query1), PQresultErrorMessage(res));
+					PQclear(res);
+					slon_disconnectdb(conn);
+					free(conn_conninfo);
+					conn = NULL;
+					conn_conninfo = NULL;
+					rc = sched_msleep(node, pa_connretry * 1000);
+					if (rc != SCHED_STATUS_OK && rc != SCHED_STATUS_CANCEL)
+						break;
+
+					continue;
+				}
+
+			}
 			slon_log(SLON_DEBUG1,
 					 "remoteListenThread_%d: connected to '%s'\n",
 					 node->no_id, conn_conninfo);
