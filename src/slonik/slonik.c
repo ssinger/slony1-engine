5d80a61cd78b2f44e18a5d519b9c62db757355f3
Steve Singer <ssinger@ca.afilias.info>
2012-05-30 10:04:34 -0400

    Bug 268 Postgresql 9.2 (and later) has changed procpid to be pid in pg_stat_activity. Use the newer column name when connected to a new pgsql

--- src/slonik/slonik.c
+++ src/slonik/slonik.c
@@ -2867,6 +2867,7 @@ slonik_failed_node(SlonikStmt_failed_node * stmt)
 		has_candidate = true;
 		for (i = 0; i < node_entry->num_nodes; i++)
 		{
+			const char * pidcolumn;
 			nodeinfo[i].no_id = (int) strtol(PQgetvalue(res1, i, 0), NULL, 10);
 			nodeinfo[i].adminfo = get_active_adminfo((SlonikStmt *) stmt,
 													 nodeinfo[i].no_id);
@@ -2885,17 +2886,22 @@ slonik_failed_node(SlonikStmt_failed_node * stmt)
 			}
 			else
 				nodeinfo[i].failover_candidate = false;
-
+			
+			if (nodeinfo[i].adminfo->pg_version >= 90200)  
+				pidcolumn="pid";
+			else 
+				pidcolumn="procpid";
 			slon_mkquery(&query,
 						 "lock table \"_%s\".sl_config_lock; "
 						 "select nl_backendpid from \"_%s\".sl_nodelock "
 				   "    where nl_nodeid = \"_%s\".getLocalNodeId('_%s') and "
 				  "       exists (select 1 from pg_catalog.pg_stat_activity "
-						 "                 where procpid = nl_backendpid);",
+						 "                 where %s = nl_backendpid);",
 						 stmt->hdr.script->clustername,
 						 stmt->hdr.script->clustername,
 						 stmt->hdr.script->clustername,
-						 stmt->hdr.script->clustername);
+						 stmt->hdr.script->clustername,
+						 pidcolumn);
 			res3 = db_exec_select((SlonikStmt *) stmt, nodeinfo[i].adminfo, &query);
 			if (res3 == NULL)
 			{
