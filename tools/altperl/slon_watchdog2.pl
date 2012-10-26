f95673808cc7704203be6a49bb1168425c7ccfa2
Luiz K. Matsumura <luiz.matsumura@gmail.com>
2012-10-26 18:53:13 -0200

    New Watchdog options - verbosity
    Implementing new parameters on slon_tools.conf
    WATCHDOG_VERSION : select watchdog to start
       1: slon_watchdog
       2: slon_watchdog2
    
    WATCHDOG_VERBOSE: enable/disable verbosity of watchdog logs
    
    START_WATCHDOG and SLEEP_TIME: just declaring since the funcionality
      was already implemented
    
    
    slon_watchdog2: making the routine functional since some bugs was detected,
      routine updating as changes detected on slon_watchdog

--- tools/altperl/slon_watchdog2.pl
+++ tools/altperl/slon_watchdog2.pl
@@ -3,35 +3,68 @@
 # Author: Christopher Browne
 # Copyright 2004-2009 Afilias Canada
 
+use Getopt::Long;
+
+# Defaults
+$CONFIG_FILE = '@@SYSCONFDIR@@/slon_tools.conf';
+$SHOW_USAGE  = 0;
+$WATCHDOG_VERBOSE = 1;
+
+# Read command-line options
+GetOptions("config=s"  => \$CONFIG_FILE,
+           "help"      => \$SHOW_USAGE);
+
+my $USAGE =
+"Usage: slon_watchdog2 [--config file] node# sleep_seconds
+
+    --config file  Location of the slon_tools.conf file
+
+    sleep_seconds  Number of seconds for the watchdog process to sleep
+                   between checks
+
+";
+
+if ($SHOW_USAGE or scalar(@ARGV) != 2) {
+  die $USAGE;
+}
+
 require '@@PERLSHAREDIR@@/slon-tools.pm';
-require '@@SYSCONFDIR@@/slon_tools.conf';
+require $CONFIG_FILE;
 
-$node =$ARGV[0];
-$sleep =$ARGV[1];
+$node  = $ARGV[0];
+$sleep = $ARGV[1];
 
-if ( scalar(@ARGV) < 2 ) {
-  die "Usage: ./slon_watchdog node sleep-time\n";
-}
-
-if ($node =~/^node(\d+)$/) {
+if ($node =~ /^(?:node)?(\d+)$/) {
+  $node = "node$1";
   $nodenum = $1;
+} else {
+  die $USAGE;
 }
 
-log_to_watchdog_log("Invoking watchdog for $CLUSTER_NAME node $nodenum");
+my $logfile = "$LOGDIR/slon-watchdog.log";
+
+log_to_file( $logfile , "Invoking watchdog for $CLUSTER_NAME node $nodenum, sleep time = $sleep +/- " . int($sleep/2) . " seconds");
+
 while (1) {
   my $res = query_slony_status($nodenum);    # See where the node stands
   my $eventsOK;
   if ($res =~ /^\s*t\s*\|/) {
     $eventsOK = "YES";
+    if ( $WATCHDOG_VERBOSE ) {
+      log_to_file( $logfile , "Query_slony_status returns true for $CLUSTER_NAME node $nodenum" );
+    }
   } else {
-    $res = node_is_subscribing();
+    $res = node_is_subscribing($nodenum);
     if ($res =~ /SUBSCRIBE_SET/) {
       $eventsOK = "YES";
     } else {
       $eventsOK = "NO";
     }
+    if ( $WATCHDOG_VERBOSE ) {
+      log_to_file( $logfile , "Query_slony_status returns false for $CLUSTER_NAME node $nodenum, node_is_subscribing : $eventsOK" );
+    }
   }
-  my $pid = get_pid($node);                  # See if the slon process is alive
+  my $pid = get_pid($node);  # See if the slon process is alive
   my ($restart, $kick);
   $kick = "NO";   # Initially, assume we don't need to submit a "restart node" command
   if ($pid) {  # PID is alive...
@@ -43,25 +76,34 @@ while (1) {
     }
   } else {
     $restart = "YES";
-    # See if the slon log ends with "FATAL  localListenThread: Another slon daemon is serving this node already"
-    my $lastlog=`/bin/ls -t $LOGDIR/node$nodenum/$dbname*log | head -1`;
-    my $lastline=`tail -1 $lastlog`;
-    if ($lastline =~ /Another slon daemon is serving this node already/) {
-      $kick = "YES";   # Yup, need to tell slonik to reset this node
-    }
+
+#  The message searched isn't generated as bellow anymore...
+#  and may be exist another better way to know if the restart of node is necessary
+#  so this is a TODO, commenting the code since this is not working
+#
+#    # See if the slon log ends with "FATAL  localListenThread: Another slon daemon is serving this node already"
+#    my $lastlog=`/bin/ls -t $LOGDIR/node$nodenum/$dbname*log | head -1`;
+#    my $lastline=`tail -1 $lastlog`;
+#    if ($lastline =~ /Another slon daemon is serving this node already/) {
+#      $kick = "YES";   # Yup, need to tell slonik to reset this node
+#    }
+
+    # Kicking allways as slon_watchdog.pl do
+    $kick = "YES";
   }
 
   # If the node needs a swift kick in the "RESTART", then submit that to slonik
   if ($kick eq "YES") {
-    log_to_watchdog_log("submit slonik to restart $CLUSTER_NAME node $nodenum");
-    open(SLONIK, "|@@SLONBINDIR@@/slonik");
-    print SLONIK genheader();
-    print SLONIK "restart node $node\n";
-    close SLONIK;
+    log_to_file($logfile,"submit slonik to restart $CLUSTER_NAME node $nodenum");
+    if ($CONFIG_FILE ne "") {
+      system "(@@TOOLSBIN@@/slonik_restart_node --config=${CONFIG_FILE} $node | @@SLONBINDIR@@/slonik) >> $logfile 2>> $logfile";
+    } else {
+      system "(@@TOOLSBIN@@/slonik_restart_node $node | @@SLONBINDIR@@/slonik) >> $logfile 2>> $logfile";
+    }
   }
   if ($restart eq "YES") {
     if ($pid) {
-      log_to_watchdog_log("terminate slon daemon for $CLUSTER_NAME node $nodenum");
+      log_to_file($logfile,"terminate slon daemon for $CLUSTER_NAME node $nodenum, PID $pid");
       # Kill slon until dead...
       kill 2, $pid;
       sleep 3;
@@ -69,18 +111,18 @@ while (1) {
       sleep 3;
       kill 9, $pid;
     }
-    log_to_watchdog_log("restart slon for $nodenum");
+    log_to_file($logfile,"restart slon for $CLUSTER_NAME node $nodenum");
     start_slon($nodenum);
   }
-  sleep $sleep;
+  sleep $sleep + (rand($sleep) - $sleep/2);
 }
 
-sub log_to_watchdog_log {
-  my ($message) = @_;
+sub log_to_file {
+  my ($logfile,$message) = @_;
   chomp $message;
   my $date = `date`;
   chomp $date;
-  open (SLONLOG, ">>$LOGDIR/slony-watchdog.log");
+  open (SLONLOG, ">>$logfile");
   print SLONLOG $date, "|", $message, "\n";
   close SLONLOG;
 }
