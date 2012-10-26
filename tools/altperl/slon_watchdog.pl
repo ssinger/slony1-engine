b895e9c224ad0fa2ec27e079b381f3eb23b255a8
Luiz K. Matsumura <luiz.matsumura@gmail.com>
2012-10-26 18:53:13 -0200

    New Watchdog options - verbosity
    Implementing new parameters on slon_tools.conf
    WATCHDOG_VERSION : select watchdog to start
       1: slon_watchdog
       2: slon_watchdog2
    
    WATCHDOG_VERBOSE: enable/disable verbosity of watchdog logs
    
    START_WATCHDOG and SLEEP_TIME: just declaring since the funcionality
      was already implemented
    
    slon_watchdog2: making the routine functional since some bugs was detected,
      routine updating as changes detected on slon_watchdog

--- tools/altperl/slon_watchdog.pl
+++ tools/altperl/slon_watchdog.pl
@@ -8,6 +8,7 @@ use Getopt::Long;
 # Defaults
 $CONFIG_FILE = '@@SYSCONFDIR@@/slon_tools.conf';
 $SHOW_USAGE  = 0;
+$WATCHDOG_VERBOSE = 1;
 
 # Read command-line options
 GetOptions("config=s"  => \$CONFIG_FILE,
@@ -33,39 +34,59 @@ require $CONFIG_FILE;
 $node = $ARGV[0];
 $sleep = $ARGV[1];
 
-if ($node =~/^(?:node)?(\d+)$/) {
+if ($node =~ /^(?:node)?(\d+)$/) {
+  $node = "node$1";
   $nodenum = $1;
 } else {
   die $USAGE;
 }
 
+my $logfile = "$LOGDIR/slon-watchdog.log";
+my $logfile_err = "$LOGDIR/slon-$CLUSTER_NAME-$node.err";
+
+log_to_file( $logfile , "Invoking watchdog for $CLUSTER_NAME node $nodenum , sleep time = $sleep +/- " . int($sleep/2) . " seconds");
+
 while (1) {
   $pid = get_pid($node);
   if (!($pid)) {
     my ($dsn, $dbname) = ($DSN[$nodenum], $DBNAME[$nodenum]);
-    my ($logfile) = "$LOGDIR/slon-$dbname-$node.err";
-    open (SLONLOG, ">>$logfile");
-    print SLONLOG "WATCHDOG: No Slon is running for node $node!\n";
-    print SLONLOG "WATCHDOG: You ought to check the postmaster and slon for evidence of a crash!\n";
-    print SLONLOG "WATCHDOG: I'm going to restart slon for $node...\n";
+    my $message = qq{WATCHDOG: No Slon is running for $CLUSTER_NAME node $nodenum!
+You ought to check the postmaster and slon for evidence of a crash!
+I'm going to restart slon for $node...
+};
+
+    # log to error log as original routine
+    log_to_file( $logfile_err , $message );
+
+    # log to watchdog log
+    log_to_file( $logfile , "No Slon running for $CLUSTER_NAME node $nodenum, restarting slon");
+
     # First, restart the node using slonik
     if ($CONFIG_FILE ne "") {
-      system "(@@TOOLSBIN@@/slonik_restart_node --config=${CONFIG_FILE} $node | @@SLONBINDIR@@/slonik) >> $logfile 2>> $logfile";
+      system "(@@TOOLSBIN@@/slonik_restart_node --config=${CONFIG_FILE} $node | @@SLONBINDIR@@/slonik) >> $logfile_err 2>> $logfile_err";
     } else {
-      system "(@@TOOLSBIN@@/slonik_restart_node $node | @@SLONBINDIR@@/slonik) >> $logfile 2>> $logfile";
+      system "(@@TOOLSBIN@@/slonik_restart_node $node | @@SLONBINDIR@@/slonik) >> $logfile_err 2>> $logfile_err";
     }
     # Next, restart the slon process to service the node
     start_slon($nodenum);
     $pid = get_pid($node);
-    print SLONLOG "WATCHDOG: Restarted slon for the $CLUSTER_NAME cluster $node, PID $pid\n";
+    log_to_file( $logfile_err , "WATCHDOG: Restarted slon for the $CLUSTER_NAME cluster $node, new PID $pid" );
   } else {
-    open(LOG, ">>$LOGDIR/slon_watchdog.log");
-    print LOG "\n";
-    system "date >> $LOGDIR/slon_watchdog.log";
-    print LOG "Found slon daemon running for the $CLUSTER_NAME cluster $node, PID $pid\n";
-    print LOG "Looks Ok\n";
-    print LOG "Sleeping for $sleep +/- " . int($sleep/2) . " seconds\n";
+    if ( $WATCHDOG_VERBOSE ) {
+      log_to_file( $logfile , "Found slon daemon for $CLUSTER_NAME $node, PID $pid, Sleeping for $sleep +/- " . int($sleep/2) . " seconds" );
+    }
   }
-  close(PSOUT);
   sleep $sleep + (rand($sleep) - $sleep/2);
 }
+
+sub log_to_file {
+  my ($logfile,$message) = @_;
+  chomp $message;
+  my $date = `date`;
+  chomp $date;
+
+  open (SLONLOG, ">>$logfile");
+  print SLONLOG $date, " | ", $message, "\n";
+  close SLONLOG;
+}
+
