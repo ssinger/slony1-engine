b62dcbbe279db97b0fdecfd4a583a8ce4d8c43cb
Christopher Browne <cbbrowne@ca.afilias.info>
2012-11-30 17:18:10 -0500

    Add in code to recognize PostgreSQL 9.2 and accept it as a supported version, rather than warning that we're not sure

--- src/slonik/slonik.c
+++ src/slonik/slonik.c
@@ -1905,15 +1905,15 @@ load_slony_base(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;   
 	}		
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90200)) /* 9.x */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90300)) /* 9.x */
 	{
 		/**
-		 * 9.0 and 9.1 are so far just like 8.4
+		 * 9.0 and 9.1 and 9.2 are so far just like 8.4
 		 **/
 		use_major=8;
 		use_minor=4;
 	}
-	else	/* above 9.1 ??? */
+	else	/* above 9.2 ??? */
 	{
 		use_major = 8;
 		use_minor = 4;
@@ -1990,10 +1990,10 @@ load_slony_functions(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;
 	}
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90200)) /* 9.0, 9.1 */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90300)) /* 9.0, 9.1, 9.2 */
 	{
 		/**
-		 * 9.0 and 9.1 are so far just like 8.4
+		 * 9.0 and 9.1 and 9.2 are so far just like 8.4
 		 */
 		use_major = 8;
 		use_minor = 4;
