b5b988dd570bc1cf0a1d367e43f55c60e768d852
Steve Singer <ssinger@ca.afilias.info>
2013-11-02 14:56:47 -0400

    updating version for 2.2.1

--- src/backend/slony1_funcs.sql
+++ src/backend/slony1_funcs.sql
@@ -477,7 +477,7 @@ create or replace function @NAMESPACE@.slonyVersionPatchlevel()
 returns int4
 as $$
 begin
-	return 0;
+	return 1;
 end;
 $$ language plpgsql;
 comment on function @NAMESPACE@.slonyVersionPatchlevel () is 
