30fbf4a66057da1b8148bbf3a4d2220011bda8d0
Steve Singer <ssinger@ca.afilias.info>
2016-05-28 17:41:42 -0400

    add supported compatibility for PG 9.5

--- src/slonik/slonik.c
+++ src/slonik/slonik.c
@@ -2076,7 +2076,7 @@ load_slony_base(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;   
 	}		
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90500)) /* 9.x */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90600)) /* 9.x */
 	{
 		/**
 		 * 9.0 and 9.1 and 9.2, 9.3,9.4 are so far just like 8.4
@@ -2165,7 +2165,7 @@ load_slony_functions(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;
 	}
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90500)) /* 9.0, 9.1, 9.2 */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 90600)) /* 9.0, 9.1, 9.2 */
 	{
 		/**
 		 * 9.0 and 9.1 and 9.2 and 9.3,9.4 are so far just like 8.4
