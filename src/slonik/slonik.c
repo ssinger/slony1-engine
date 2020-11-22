6a8c7d822d8a62f15faf5970d75e77eeac89d7b6
Steve Singer <steve@ssinger.info>
2020-11-22 11:59:28 -0500

    Remove unsupported warning on PG13 Bump version to 2.2.9
    Patch by Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>

--- src/slonik/slonik.c
+++ src/slonik/slonik.c
@@ -2089,15 +2089,15 @@ load_slony_base(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;
 	}
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 120000)) /* 12 */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 130000)) /* 13 */
 	{
 		/**
-		 * 9.0 through 12 are so far just like 8.4
+		 * 9.0 through 13 are so far just like 8.4
 		 **/
 		use_major = 8;
 		use_minor = 4;
 	}
-	else	/* above 12 */
+	else	/* above 13 */
 	{
 		use_major = 8;
 		use_minor = 4;
@@ -2178,7 +2178,7 @@ load_slony_functions(SlonikStmt * stmt, int no_id)
 		use_major = 8;
 		use_minor = 4;
 	}
-	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 120000)) /* 9.0, 9.1, 9.2 */
+	else if ((adminfo->pg_version >= 90000) && (adminfo->pg_version < 130000)) /* 9.0, 9.1, 9.2 */
 	{
 		/**
 		 * 9.0 and 9.1 and 9.2 and 9.3,9.4 are so far just like 8.4
